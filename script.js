9ca90cc5637de1115a78cff6c5d123326985bcb3
Tomasz Wilczynski <wilkubone@gmail.com>
2023-11-05 18:00:11 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const arrowBtn = document.querySelector('.arrow')
+const arrowIcon = document.querySelector('.fa-solid')
+const img = document.querySelector('.item1')
+
+const showImg = () => { 
+    img.classList.toggle('hide')
+
+    if(img.classList.contains('hide')) {
+        arrowIcon.style.transform = 'rotate(180deg)'
+    }else{
+        arrowIcon.style.transform = 'rotate(0)'
+    }
+ }
+
+ arrowBtn.addEventListener('click', showImg) 
